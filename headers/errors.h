63664ac3e525dc76adfc6e4547f59ebd7b1fb24d
Maboey <manuel.martinbalmori@gmail.com>
2025-09-09 20:03:25 +0200

    added check_file_path function to check name+extension of file is valid

--- headers/errors.h
+++ headers/errors.h
@@ -6,7 +6,7 @@
 /*   By: manuelma <manuelma@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 22:51:10 by manuelma          #+#    #+#             */
-/*   Updated: 2025/09/09 01:09:27 by manuelma         ###   ########.fr       */
+/*   Updated: 2025/09/09 19:59:17 by manuelma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ typedef enum e_error
 {
 	SUCCESS = 0,
 	ERR_ARGS,
-	ERR_READ
+	ERR_READ,
+	ERR_FILE_NAME,
+	ERR_MISS_OR_INVAL_PARAM
 }	t_error;
 
 # define MSG_ERR_ARGS "Error\nIncorrect args, you must add one argument\
--- src/map_parsing/map_parser.c
+++ src/map_parsing/map_parser.c
@@ -6,7 +6,7 @@
 /*   By: manuelma <manuelma@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 00:25:42 by manuelma          #+#    #+#             */
-/*   Updated: 2025/09/09 01:42:20 by manuelma         ###   ########.fr       */
+/*   Updated: 2025/09/09 20:02:28 by manuelma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,25 @@ int	get_map_param(char **file_lines, t_map_data *map_data)
 				nbr_of_param_found++;
 		i_lines++;
 	}
-	// return index of last parameter line it got, or -1 if missing parametters, or invalid one
+	// return index of last parameter line it got, or -1 if missing parametters, or invalid one detected
+}
+
+/**
+ * @brief checks that the file path is at least a char followed by '.cub'
+ * @param file_path the map file path
+ * @return ERR_FILE_NAME in case of error or SUCCESS
+ */
+int	check_file_path(char *file_path)
+{
+	int	i;
+
+	i = ft_strlen(file_path);
+	if (i <= 4)
+		return (ERR_FILE_NAME);
+	if (file_path[i - 4] != '.' || file_path[i - 3] != 'c' \
+		|| file_path[i - 2] != 'u' || file_path[i - 1] != 'b')
+		return (ERR_FILE_NAME);
+	return (SUCCESS);
 }
 
 /**
@@ -45,12 +63,17 @@ int	get_map_param(char **file_lines, t_map_data *map_data)
 int	map_parser(char *file_path, t_map_data *map_data)
 {
 	char	**file_lines;
+	int		last_param_index;
 
-	// first check file name extension !!
-	
+	if (check_file_path(file_path) != SUCCESS)
+		return (ERR_FILE_NAME);
 	file_lines = get_file_lines(file_path);
 	if (file_lines == NULL)
 		return (ERR_READ);
-	free_double(file_lines);
+	last_param_index = get_map_param(file_lines, map_data);
+	if (last_param_index == -1)
+		return (free_double((void ***)&file_lines), ERR_MISS_OR_INVAL_PARAM);
+	//check and store map after last_param_index
+	free_double((void ***)&file_lines);
 	return (SUCCESS);
 }
