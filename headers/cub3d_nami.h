a364e71cd95cb2d29f72ac308f09a92a75c09008
Jeong Nam Lee <jelee@c2r4s4.42lausanne.ch>
2025-10-07 16:56:26 +0200

    fixed keyboard into linux keyboard

--- headers/cub3d_nami.h
+++ headers/cub3d_nami.h
@@ -5,13 +5,13 @@
 #include <string.h> // Requis pour memcpy
 
 // Le keycode pour la touche ESC sur macOS (65307 sur Linux)
-#define KEY_ESC 53
-#define KEY_W 13
-#define KEY_A 0
-#define KEY_S 1
-#define KEY_D 2
-#define KEY_LEFT 123
-#define KEY_RIGHT 124
+# define KEY_A 97
+# define KEY_S 115
+# define KEY_D 100
+# define KEY_W 119
+# define KEY_ESC 65307
+# define KEY_LEFT 65361
+# define KEY_RIGHT 65363
 
 #define MAP_WIDTH 8
 #define MAP_HEIGHT 8
